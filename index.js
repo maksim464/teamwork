b3b4d17b6aaae483e5884243f6af960fae2f0f7d
bulavko marina <martijbcde@gmail.com>
2024-12-15 14:55:11 +0200

    added filters

--- index.js
+++ index.js
@@ -7,53 +7,49 @@ class Todo {
         this.status = status;
         this.updated = updated;
         this.html = this.generateHtml();
-        todoList.push(this); 
+        todoList.push(this);
     }
 
     generateHtml() {
         return `
-<li class="todos_todo" data-id="${this.id}">
- <span class="todo__checkbox toggle-status" draggable="false" style="user-select:none; cursor:pointer">${this.status ? "✔" : "☐"}</span> 
-    <p class="todo__title">${this.id}. ${this.name}${this.updated ? " (updated)" : ""}</p>
-    <div class="todo__btn-container">
-        <button class="todo__btn edit">Редагувати</button>
-        <button class="todo__btn remove">Видалити</button>
-    </div>
-</li>`;
+        <li class="todos_todo" data-id="${this.id}">
+            <span class="todo__checkbox toggle-status" draggable="false" style="user-select:none; cursor:pointer">${this.status ? "✔" : "☐"}</span> 
+            <p class="todo__title">${this.id}. ${this.name}${this.updated ? " (updated)" : ""}</p>
+            <div class="todo__btn-container">
+                <button class="todo__btn edit">Редагувати</button>
+                <button class="todo__btn remove">Видалити</button>
+            </div>
+        </li>`;
     }
 
- 
     edit(newName) {
         this.name = newName;
         this.updated = true;
-        this.html = this.generateHtml(); 
+        this.html = this.generateHtml();
     }
 
-  
     toggleStatus() {
         this.status = !this.status;
-        this.html = this.generateHtml(); 
+        this.html = this.generateHtml();
     }
 
-   
     remove() {
         const index = todoList.findIndex(todo => todo.id === this.id);
         if (index !== -1) {
-            todoList.splice(index, 1); 
+            todoList.splice(index, 1);
         }
     }
 }
 
-
 function updateUncheckedList() {
     const uncheckContainer = document.querySelector('.uncheck-container__actuals');
-    uncheckContainer.innerHTML = ''; 
+    uncheckContainer.innerHTML = '';
 
     const uncheckedTodos = todoList.filter(todo => !todo.status);
 
     for (let index = 0; index < uncheckedTodos.length; index++) {
-        if (index === 10) {
-            break; 
+        if (index >= 10) {
+            break;
         }
 
         const todo = uncheckedTodos[index];
@@ -66,33 +62,26 @@ function updateUncheckedList() {
     }
 }
 
-
 function saveTodosToLocalStorage() {
     const json = JSON.stringify(todoList);
-    localStorage.setItem('todos', json); 
+    localStorage.setItem('todos', json);
 }
 
-
 function loadTodosFromLocalStorage() {
-    const json = localStorage.getItem('todos'); 
+    const json = localStorage.getItem('todos');
     if (json) {
         const todos = JSON.parse(json);
 
-        
         const todoListContainer = document.querySelector('.todo-list');
-
-        
         todoListContainer.innerHTML = '';
 
-       
         todos.forEach(todo => {
             const { id, name, status, updated } = todo;
             const newTodo = new Todo(id, name, status, updated);
-            todoListContainer.innerHTML += newTodo.html; 
+            todoListContainer.innerHTML += newTodo.html;
         });
     }
 }
-
 
 function requestNotificationPermission() {
     if (Notification.permission !== "granted") {
@@ -100,34 +89,58 @@ function requestNotificationPermission() {
     }
 }
 
-
 function sendNotification(title, body) {
     if (Notification.permission === "granted") {
         new Notification(title, { body });
     }
 }
 
-
 function setDailyReminder(todo) {
-    const reminderTime = 24 * 60 * 60 * 1000; 
+    const reminderTime = 24 * 60 * 60 * 1000;
     setTimeout(() => {
         sendNotification("Нагадування", `Завдання: ${todo.name}`);
-        setDailyReminder(todo); 
+        setDailyReminder(todo);
     }, reminderTime);
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
     requestNotificationPermission();
-    loadTodosFromLocalStorage(); 
+    loadTodosFromLocalStorage();
     updateUncheckedList();
     todoList.forEach(todo => {
         if (!todo.status) {
-            setDailyReminder(todo); 
+            setDailyReminder(todo);
         }
     });
 });
 
+
+function filterTodos(filter) {
+    const todoListContainer = document.querySelector('.todo-list');
+    todoListContainer.innerHTML = ''; 
+
+    let filteredTodos;
+
+    switch (filter) {
+        case 'all':
+            filteredTodos = todoList;
+            break;
+        case 'checked':
+            filteredTodos = todoList.filter(todo => todo.status);
+            break;
+        case 'unchecked':
+            filteredTodos = todoList.filter(todo => !todo.status);
+            break;
+        default:
+            filteredTodos = todoList;
+    }
+
+    filteredTodos.forEach(todo => {
+        todoListContainer.innerHTML += todo.html;
+    });
+
+    updateUncheckedList(); 
+}
 
 document.querySelector('.add').addEventListener('click', () => {
     const inputElement = document.querySelector('.text__input');
@@ -135,10 +148,10 @@ document.querySelector('.add').addEventListener('click', () => {
     if (todoName) {
         const newTodo = new Todo(todoList.length + 1, todoName);
         document.querySelector('.todo-list').innerHTML += newTodo.html;
-        updateUncheckedList(); 
+        updateUncheckedList();
         saveTodosToLocalStorage();
         setDailyReminder(newTodo);
-        inputElement.value = ''; 
+        inputElement.value = '';
     }
 });
 
@@ -152,16 +165,20 @@ document.querySelector('.todo-list').addEventListener('click', (event) => {
         const newName = prompt("Введіть нову назву задачі:", todo.name);
         if (newName) {
             todo.edit(newName);
-            todoElement.outerHTML = todo.html; 
+            todoElement.outerHTML = todo.html;
         }
     } else if (button.classList.contains('remove')) {
         todo.remove();
         todoElement.remove();
     } else if (button.classList.contains('toggle-status')) {
         todo.toggleStatus();
-        todoElement.outerHTML = todo.html; 
+        todoElement.outerHTML = todo.html;
     }
 
-    updateUncheckedList(); 
-    saveTodosToLocalStorage(); 
+    updateUncheckedList();
+    saveTodosToLocalStorage();
 });
+
+document.querySelector('.filter.all').addEventListener('click', () => filterTodos('all'));
+document.querySelector('.filter.checked').addEventListener('click', () => filterTodos('checked'));
+document.querySelector('.filter.unchecked').addEventListener('click', () => filterTodos('unchecked'));
